4483b86925b0d20f8c8997f62bf7850c5685b0a7
M. Todd MATTHEWS <mtoddmatthews@gmail.com>
2017-07-15 12:39:25 -0400

    fixed issue #18 - setup.py compliance w/ pep8

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ def read(fname):
 
 
 setup(
-    name = "sprint_tutorial",
-    version = "0.0.1",
-    author = "Jonathan Rocher",
-    author_email = "jonathanrocher@gmail.com",
-    description = ("Sample repository to learn to sprint on open source "
-                   "software"),
-    license = "BSD",
-    keywords = "example sprint tutorial",
-    url = "https://github.com/jonathanrocher/sprint_tutorial",
+    name="sprint_tutorial",
+    version="0.0.1",
+    author="Jonathan Rocher",
+    author_email="jonathanrocher@gmail.com",
+    description=("Sample repository to learn to sprint on open source "
+                 "software"),
+    license="BSD",
+    keywords="example sprint tutorial",
+    url="https://github.com/jonathanrocher/sprint_tutorial",
     packages=['sprint_tutorial'],
     long_description=read('README.md'),
     classifiers=[
